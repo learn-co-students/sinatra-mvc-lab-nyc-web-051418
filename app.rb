5abd07c8c74aa96422126a711781f2cabfba38dd
github username <github email address>
2018-06-06 15:28:32 -0400

    Done.

--- app.rb
+++ app.rb
@@ -6,16 +6,24 @@ class App < Sinatra::Base
       erb :user_input
     end
 
+    post '/piglatinize' do
 
-    post '/rearrange' do
-      #receive params here
-      # @pig_instance = PigLatinizer.new(params[:input])
-      # @pig_instance.rearrange_string
-
-      english_phrase = PigLatinizer.new(params[:input])
-      @pig_latin_phrase = english_phrase.rearrange_string
-
+      piglatinizer = PigLatinizer.new()
+      @piglatinize = piglatinizer.piglatinize(params[:user_phrase])
       erb :user_output
+
+      # string_input = PigLatinizer.new
+      #
+      # string_to_alter = params[:user_input]
+      #
+      # if string_to_alter =~ /\s/
+      #   @piglatinize = string_input.piglan
+      #   (string_to_alter)
+      # else
+      #   @piglatinize = string_input.rearrange_string(string_to_alter)
+      # end
+
+
     end
 
 
--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,83 +1,33 @@
 class PigLatinizer
 
-  attr_accessor :string
-
-  def initialize(string)
-    @string = string.downcase
-  end
-
   def find_first_consonant(original_word)
     word_array = original_word.chars
     first_vowel_index = word_array.find_index do |letter|
-        letter =~ (/[aeiou]/)
+        letter =~ (/[aAeEiIoOuU]/)
     end
     first_vowel_index
   end
 
-  def rearrange_string
-
-    vowel_location = find_first_consonant
-
+  def rearrange_string(word_to_change)
+    vowel_location = find_first_consonant(word_to_change)
     if vowel_location > 0
-        # pig_word_start = @string.slice!(0,vowel_location)
-        # @string << pig_word_start
-        pig_word = @string.slice(0,vowel_location) + @string.slice(vowel_location, 0)
-        add_ay(pig_word)
+        pig_word = word_to_change.slice!(0,vowel_location)
+        pig_latin = word_to_change << pig_word
+        add_ay(pig_latin)
     else
-        pig_word = @string
+        word_to_change + "way"
     end
-
   end
-
 
   def add_ay(pig_word)
     pig_word << "ay"
   end
 
-
-
-end
-
-
-
-class PigLatinizer
-
-  # attr_accessor :string
-
-  # def initialize(string)
-  #   @string = string.downcase
-  # end
-
-  # def find_first_consonant(original_word)
-  #   word_array = original_word.chars
-  #   first_vowel_index = word_array.find_index do |letter|
-  #       letter =~ (/[aeiou]/)
-  #   end
-  #   first_vowel_index
-  # end
-
-  def rearrange_string#(pig_word)
-
-    pig_word + "!!!"
-
-    # vowel_location = find_first_consonant(pig_word)
-    #
-    # if vowel_location > 0
-    #     # pig_word_start = @string.slice!(0,vowel_location)
-    #     # @string << pig_word_start
-    #     string_wihout_ay = pig_word.slice(0,vowel_location) + pig_word.slice(vowel_location, 0)
-    #     add_ay(string_wihout_ay)
-    # else
-    #     pig_word
-    # end
-
+  def piglatinize(complete_sentence)
+    changed_sentence = complete_sentence.split(" ").map do |word|
+        rearrange_string(word)
+    end
+    changed_sentence.join(" ")
   end
-
-  #
-  # def add_ay(to_change)
-  #   pig_word << "ay"
-  # end
-  #
-
 
 end
